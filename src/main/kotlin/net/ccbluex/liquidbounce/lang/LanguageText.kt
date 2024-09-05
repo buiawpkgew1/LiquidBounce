f7d47d7885723278c8b0a6ebf136ec5603943e02
菾凴 <buiawpkgew1@outlook.com>
2024-09-05 21:42:42 +0800

    feat: 添加LanguageText类以支持多语言文本处理

--- src/main/kotlin/net/ccbluex/liquidbounce/lang/LanguageText.kt
+++ src/main/kotlin/net/ccbluex/liquidbounce/lang/LanguageText.kt
@@ -2,5 +2,7 @@ package net.ccbluex.liquidbounce.lang
 
 import net.minecraft.text.TranslatableTextContent
 
+// 定义一个名为 LanguageText 的类，继承自 TranslatableTextContent
+// 该类用于处理多语言文本的翻译内容
 class LanguageText(key: String, args: Array<out Any>) :
     TranslatableTextContent(key, null, args)
